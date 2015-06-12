fd5835194db7d253b9918f432469320170f5ccc0
Sadagopan <sadagopannscbe@gmail.com>
2015-06-12 12:53:20 +0530

    Modified: Added HSS

---
+++ hss.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include "hss.h"
+#include "server.c"
+
+DB::DB(){
+	details.server = "localhost";
+	details.user = "root";
+	details.passwd = "mysql";
+	details.database = "NFV";
+}
+
+void DB::print_message(const char *message){
+	cout<<"***********************"<<endl;
+	cout<<message<<endl;
+	cout<<"***********************"<<endl;
+}
+
+void DB::conn_setup(){
+	sql.conn = mysql_init(NULL);;
+	if(!mysql_real_connect(sql.conn,details.server,details.user,details.passwd,details.database,0,NULL,0)){
+		cout<<mysql_error(sql.conn)<<endl;
+		exit(1);
+	}
+	print_message("Connected to database");
+}
+
+void DB::perform_query(const char *query){
+	if(mysql_query(sql.conn, query)){
+		cout<<mysql_error(sql.conn)<<endl;
+		exit(1);
+	}
+	sql.result = mysql_store_result(sql.conn);
+}
+
+void DB::fetch_result(){
+	int num_fields;
+	MYSQL_ROW row;
+	num_fields = mysql_num_fields(sql.result);
+	print_message("QUERY RESULT");
+	while(row = mysql_fetch_row(sql.result)){
+		for(int i=0;i<num_fields;i++)
+			cout<<(row[i]?row[i]:"NULL")<<" ";
+		cout<<endl;	
+	}
+}
+
+DB::~DB(){
+	mysql_free_result(sql.result);
+	mysql_close(sql.conn);
+}
+
+void fetch_DBdata(const char *query){
+	DB db;
+	db.conn_setup();
+	db.perform_query(query);
+	db.fetch_result();
+}
+
+int main(){
+	SERVER server;
+	server.start_listening();
+	server.accept_connections();
+	fetch_DBdata("SELECT * FROM UE");
+	return 0;
+}
---
+++ server.h
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <signal.h>
+
+#include <pthread.h>
+#include <netdb.h> 
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <sys/types.h>
+using namespace std;
+
+#define MAX_CONNECTIONS 100
+
+class SERVER{
+	public:
+		int listen_port;
+		int listen_socket;
+		int client_socket[MAX_CONNECTIONS];
+		pthread_t tid[MAX_CONNECTIONS];
+		struct sockaddr_in server_addr;
+		struct sockaddr_in client_addr;
+		socklen_t client_addr_len; 
+	
+		SERVER();
+		void start_listening();
+		void accept_connections();
+		void* multithreading_func(void*);
+		void report_error(int);
+		void print_message(const char*);
+		~SERVER();
+};
